d5bd55076ef0037f3e3d890e06adb407fed04478
SourabhIngawale <ingawalesourabh@gmail.com>
2023-09-26 16:42:18 +0530

    small changes

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var index = 0;
 function  changeColour(){
 
     var colour=["red","yellow","blue","purple","green"];
-    document.getElementsByTagName("body")[0].
-    style.background = colour[index++];
+    document.getElementsByTagName("body")[0].style.background = colour[index++];
     if(index > colour.length -1)
     index=0;
 
