3de7fb48ed13c0caf48a8acef4bf835c4e7bfdd3
SourabhIngawale <ingawalesourabh@gmail.com>
2023-09-26 16:35:51 +0530

    change background basic project

---
+++ script.js
@@ -0,0 +1,11 @@
+var index = 0;
+function  changeColour(){
+
+    var colour=["red","yellow","blue","purple","green"];
+    document.getElementsByTagName("body")[0].
+    style.background = colour[index++];
+    if(index > colour.length -1)
+    index=0;
+
+
+}
